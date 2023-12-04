3edf5e932ca89e5e01c2fb202ec29709f7bdef76
Peter O'Neill <peter.oneill@metaswitch.com>
2023-12-04 11:39:01 +0000

    solve d4 p2

--- src/d4/mod.rs
+++ src/d4/mod.rs
@@ -41,11 +41,47 @@ impl Solver for Solver4 {
     }
 
     fn part2(&self, input_lines: Lines) -> String {
-        let mut total = 0;
+        let mut card_count: Vec<usize> = Vec::new();
 
-        total += 1;
+        let number_matcher = regex::Regex::new(r"\d+").unwrap();
 
-        total.to_string()
+        for (line_num, line) in input_lines.enumerate() {
+            push_or_add_to_vec(&mut card_count, line_num, 1);
+
+            let mut numbers = line.split(':').last().unwrap().split('|');
+
+            let winning_numbers = numbers.next().unwrap();
+            let winning_numbers = number_matcher
+                .find_iter(winning_numbers)
+                .map(|n| n.as_str());
+
+            let card_numbers = numbers.next().unwrap();
+            let card_numbers: Vec<&str> = number_matcher
+                .find_iter(card_numbers)
+                .map(|n| n.as_str())
+                .collect();
+
+            let number_of_matches: u32 =
+                winning_numbers.filter(|n| card_numbers.contains(n)).count() as u32;
+
+            let win_count = card_count[line_num];
+
+            for won_card_ix in line_num + 1..=line_num + number_of_matches as usize {
+                push_or_add_to_vec(&mut card_count, won_card_ix, win_count);
+            }
+        }
+
+        let mut count = 0;
+        card_count.into_iter().for_each(|n| count += n);
+        count.to_string()
+    }
+}
+
+fn push_or_add_to_vec(vec: &mut Vec<usize>, ix: usize, value: usize) {
+    if vec.len() <= ix {
+        vec.push(value);
+    } else {
+        vec[ix] += value;
     }
 }
 
@@ -64,13 +100,12 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
     }
     #[test]
     fn part2() {
-        //         let sample_input = "two1nine
-        // eightwothree
-        // abcone2threexyz
-        // xtwone3four
-        // 4nineeightseven2
-        // zoneight234
-        // 7pqrstsixteen";
-        //         assert_eq!(super::Solver4.part2(sample_input.lines()), "281");
+        let sample_input = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
+        assert_eq!(super::Solver4.part2(sample_input.lines()), "30");
     }
 }
