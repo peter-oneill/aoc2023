dcee35250a306987d137558c697baa956dbfa32c
Peter O'Neill <peter@pop-os.localdomain>
2023-12-08 10:53:02 +0000

    Got solution for day 8 using LCM, and switching to non-hashmap, probably unnecessarily

--- src/d8/mod.rs
+++ src/d8/mod.rs
@@ -1,24 +1,44 @@
-use std::{collections::HashMap, str::Lines};
+use num::{integer, traits::AsPrimitive};
+use std::{cell::RefCell, collections::HashMap, fmt::Display, str::Lines, sync::Arc};
 
 use crate::Solver;
 
 pub struct Solver8;
 
 #[derive(PartialEq, Eq, Debug)]
-struct Hand {
-    quality: i32,
-    bet: usize,
+struct Node<'a> {
+    orig_string: &'a str,
+    ends_with_z: bool,
+    left: Option<Arc<RefCell<Node<'a>>>>,
+    right: Option<Arc<RefCell<Node<'a>>>>,
 }
 
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
+impl<'a> Node<'a> {
+    fn from(line: &'a str) -> Self {
+        Node {
+            orig_string: line,
+            ends_with_z: line.chars().nth(2).unwrap() == 'Z',
+            left: None,
+            right: None,
+        }
     }
 }
 
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.quality.cmp(&other.quality)
+impl<'a> Display for Node<'a> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Node: [
+orig_string: {},
+ends_with_z: {},
+left: {},
+right: {}
+]",
+            self.orig_string,
+            self.ends_with_z,
+            self.left.as_ref().unwrap().borrow().orig_string,
+            self.right.as_ref().unwrap().borrow().orig_string
+        )
     }
 }
 
@@ -43,18 +63,8 @@ impl Solver for Solver8 {
         loop {
             for c in &directions {
                 current = match c {
-                    'L' => {
-                        // println!("current: {}", current);
-                        // println!("choosing 0");
-                        network[current].0
-                    }
-
-                    'R' => {
-                        // println!("current: {}", current);
-                        // println!("choosing 1");
-                        network[current].1
-                    }
-
+                    'L' => network[current].0,
+                    'R' => network[current].1,
                     _ => panic!("Unknown direction"),
                 };
                 steps += 1;
@@ -66,57 +76,69 @@ impl Solver for Solver8 {
     }
 
     fn part2(&self, mut input_lines: Lines) -> String {
-        let mut steps = 0;
+        let mut steps: i64 = 0;
         let directions = input_lines.next().unwrap().chars().collect::<Vec<char>>();
         let _ = input_lines.next();
 
-        let mut current_nodes: Vec<&str> = Vec::new();
-        let mut network: HashMap<&str, (&str, &str)> = HashMap::new();
-        // println!("steps: {}", steps);
+        let mut current_nodes: Vec<Arc<RefCell<Node>>> = Vec::new();
+        let mut all_nodes: HashMap<&str, Arc<RefCell<Node>>> = HashMap::new();
         for line in input_lines {
-            if line.chars().nth(2).unwrap() == 'A' {
-                current_nodes.push(&line[0..3]);
-            }
-            network.insert(&line[0..3], (&line[7..10], &line[12..15]));
+            let node = Node::from(line);
+            let key = &node.orig_string[0..3];
+            all_nodes.insert(key, Arc::new(RefCell::new(node)));
         }
-        // println!("steps: {}", steps);
+
+        for node in all_nodes.values() {
+            let mut n = node.borrow_mut();
+
+            let left = all_nodes.get(&n.orig_string[7..10]).unwrap();
+            let wrapped_left = Some(left.clone());
+            n.left = wrapped_left;
+
+            let right = all_nodes.get(&n.orig_string[12..15]).unwrap();
+            let wrapped_right = Some(right.clone());
+            n.right = wrapped_right;
+        }
+
+        for (_, n) in all_nodes {
+            if n.borrow().orig_string.chars().nth(2).unwrap() == 'A' {
+                current_nodes.push(n.clone());
+            }
+        }
 
         let num_locations = current_nodes.len();
-        // println!("steps: {}", steps);
-
+        let mut found_locs: HashMap<usize, i64> = HashMap::new();
         loop {
             for c in &directions {
-                // println!("steps: {}", steps);
+                steps += 1;
                 let mut num_to_complete = num_locations;
-                for n in current_nodes.iter_mut() {
+
+                for l in current_nodes.iter() {}
+                for (ix, n) in current_nodes.iter_mut().enumerate() {
                     *n = match c {
-                        'L' => {
-                            // println!("current: {}", n);
-                            // println!("choosing 0");
-                            network[n].0
-                        }
-                        'R' => {
-                            // println!("current: {}", n);
-                            // println!("choosing 1");
-                            network[n].1
-                        }
+                        'L' => n.borrow().left.as_ref().unwrap().clone(),
+                        'R' => n.borrow().right.as_ref().unwrap().clone(),
                         _ => panic!("Unknown direction"),
                     };
-                    // println!("n[2]: {}", n.chars().nth(2).unwrap());
-                    if n.chars().nth(2).unwrap() == 'Z' {
+                    if n.borrow().ends_with_z {
+                        if !found_locs.contains_key(&ix) {
+                            found_locs.insert(ix, steps);
+                            if found_locs.len() == num_locations {
+                                let mut vals = found_locs.values();
+                                let first = vals.next().unwrap().clone();
+                                let mut lcm = first;
+                                for s in vals {
+                                    lcm = integer::lcm(lcm, s.clone());
+                                }
+                                return lcm.to_string();
+                            }
+                        }
+
                         num_to_complete -= 1;
-                        // println!("num_to_complete: {}", num_to_complete);
                     }
                 }
-                steps += 1;
                 if num_to_complete == 0 {
                     return steps.to_string();
-                }
-                if steps % 1000000 == 0 {
-                println!("{:?} {}", current_nodes, steps);
-                if steps % 10000000 == 0 {
-                    return "10M iterations - quitting".to_string()
-                }
                 }
             }
         }
