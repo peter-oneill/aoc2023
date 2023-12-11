d3603070d194ad3485171e825ee0435ae15d2d5f
Peter O'Neill <github@onlypeter.com>
2023-12-11 06:47:54 +0000

    Commonise p1 p2 and add brief comments

--- src/d11/mod.rs
+++ src/d11/mod.rs
@@ -9,89 +9,15 @@ impl Solver for Solver11 {
     }
 
     fn part1(&self, input_lines: Lines) -> String {
-        let cloned_lines: Vec<&str> = input_lines.clone().collect();
-        let num_columns = cloned_lines[0].chars().count();
-        let num_rows = cloned_lines.len();
-
-        let mut galaxies_on_single_row: Vec<char> = vec!['.'; num_columns];
-        let mut galaxies_on_single_column: Vec<char> = vec!['.'; num_rows];
-
-        let mut galaxies: Vec<(i64, i64)> = Vec::new();
-
-        for (y, row) in input_lines.enumerate() {
-            for (x, c) in row.char_indices() {
-                if c == '#' {
-                    galaxies.push((x.try_into().unwrap(), y.try_into().unwrap()));
-                    galaxies_on_single_row[x] = '#';
-                    galaxies_on_single_column[y] = '#';
-                }
-            }
-        }
-
-        let empty_column_ixs = galaxies_on_single_row
-            .iter()
-            .enumerate()
-            .filter(|(_, c)| c == &&'.')
-            .map(|(ix, _)| ix.try_into().unwrap())
-            .collect::<Vec<i64>>();
-        let empty_row_ixs = galaxies_on_single_column
-            .iter()
-            .enumerate()
-            .filter(|(_, c)| c == &&'.')
-            .map(|(ix, _)| ix.try_into().unwrap())
-            .collect::<Vec<i64>>();
-        // 0, 5, 6, 10
-
-        // let mut x_offset = 0;
-        // let mut y_offset = 0;
-
-        let mut dist_sum = 0;
-
-        for left_ix in 0..galaxies.len() {
-            let left_g = galaxies[left_ix];
-            for right_g in galaxies[left_ix + 1..].iter() {
-                if (left_g.0 == right_g.0) && (left_g.1 == right_g.1) {
-                    continue;
-                }
-                let left_offset_x: i64 = empty_column_ixs
-                    .iter()
-                    .filter(|ix| ix < &&left_g.0)
-                    .count()
-                    .try_into()
-                    .unwrap();
-                let right_offset_x: i64 = empty_column_ixs
-                    .iter()
-                    .filter(|ix| ix < &&right_g.0)
-                    .count()
-                    .try_into()
-                    .unwrap();
-                let left_offset_y: i64 = empty_row_ixs
-                    .iter()
-                    .filter(|ix| ix < &&left_g.1)
-                    .count()
-                    .try_into()
-                    .unwrap();
-                let right_offset_y: i64 = empty_row_ixs
-                    .iter()
-                    .filter(|ix| ix < &&right_g.1)
-                    .count()
-                    .try_into()
-                    .unwrap();
-                let x_dist = ((left_g.0 + left_offset_x) - (right_g.0 + right_offset_x)).abs();
-                let y_dist = ((left_g.1 + left_offset_y) - (right_g.1 + right_offset_y)).abs();
-                dist_sum += x_dist + y_dist;
-            }
-        }
-
-        dist_sum.to_string()
+        solve_with_expansion_factor(input_lines, 2)
     }
 
     fn part2(&self, input_lines: Lines) -> String {
-        part_2_with_factor(input_lines, 1000000)
+        solve_with_expansion_factor(input_lines, 1000000)
     }
 }
 
-fn part_2_with_factor(input_lines: Lines, factor: i64) -> String {
+fn solve_with_expansion_factor(input_lines: Lines, factor: i64) -> String {
     let cloned_lines: Vec<&str> = input_lines.clone().collect();
     let num_columns = cloned_lines[0].chars().count();
     let num_rows = cloned_lines.len();
@@ -101,6 +27,9 @@ fn part_2_with_factor(input_lines: Lines, factor: i64) -> String {
 
     let mut galaxies: Vec<(i64, i64)> = Vec::new();
 
+    // 1:  for each found galaxy:
+    // - add to the list of galaxies
+    // - mark the row and column as having a galaxy
     for (y, row) in input_lines.enumerate() {
         for (x, c) in row.char_indices() {
             if c == '#' {
@@ -111,6 +40,7 @@ fn part_2_with_factor(input_lines: Lines, factor: i64) -> String {
         }
     }
 
+    // 2: get a lists of row/column indices which have no galaxies
     let empty_column_ixs = galaxies_on_single_row
         .iter()
         .enumerate()
@@ -126,37 +56,33 @@ fn part_2_with_factor(input_lines: Lines, factor: i64) -> String {
 
     let mut dist_sum = 0;
 
+    // 3: for each pair of galaxies, calculate the distance between them
     for left_ix in 0..galaxies.len() {
-        let left_g = galaxies[left_ix];
-        for right_g in galaxies[left_ix + 1..].iter() {
-            if (left_g.0 == right_g.0) && (left_g.1 == right_g.1) {
-                continue;
-            }
+        let l_gal = galaxies[left_ix];
+        for r_gal in galaxies[left_ix + 1..].iter() {
+            // The offset in each direction is the number of empty rows/columns indices which we've passed
             let left_offset_x: i64 = (factor - 1)
                 * TryInto::<i64>::try_into(
-                    empty_column_ixs.iter().filter(|ix| ix < &&left_g.0).count(),
+                    empty_column_ixs.iter().filter(|ix| ix < &&l_gal.0).count(),
                 )
                 .unwrap();
             let right_offset_x: i64 = (factor - 1)
                 * TryInto::<i64>::try_into(
-                    empty_column_ixs
-                        .iter()
-                        .filter(|ix| ix < &&right_g.0)
-                        .count(),
+                    empty_column_ixs.iter().filter(|ix| ix < &&r_gal.0).count(),
                 )
                 .unwrap();
             let left_offset_y: i64 = (factor - 1)
                 * TryInto::<i64>::try_into(
-                    empty_row_ixs.iter().filter(|ix| ix < &&left_g.1).count(),
+                    empty_row_ixs.iter().filter(|ix| ix < &&l_gal.1).count(),
                 )
                 .unwrap();
             let right_offset_y: i64 = (factor - 1)
                 * TryInto::<i64>::try_into(
-                    empty_row_ixs.iter().filter(|ix| ix < &&right_g.1).count(),
+                    empty_row_ixs.iter().filter(|ix| ix < &&r_gal.1).count(),
                 )
                 .unwrap();
-            let x_dist = ((left_g.0 + left_offset_x) - (right_g.0 + right_offset_x)).abs();
-            let y_dist = ((left_g.1 + left_offset_y) - (right_g.1 + right_offset_y)).abs();
+            let x_dist = ((l_gal.0 + left_offset_x) - (r_gal.0 + right_offset_x)).abs();
+            let y_dist = ((l_gal.1 + left_offset_y) - (r_gal.1 + right_offset_y)).abs();
             dist_sum += x_dist + y_dist;
         }
     }
@@ -166,7 +92,7 @@ fn part_2_with_factor(input_lines: Lines, factor: i64) -> String {
 
 #[cfg(test)]
 mod tests {
-    use crate::{d11::part_2_with_factor, Solver};
+    use crate::Solver;
     #[test]
     fn part1() {
         let sample_input = "...#......
@@ -194,7 +120,13 @@ mod tests {
 ..........
 .......#..
 #...#.....";
-        assert_eq!(part_2_with_factor(sample_input.lines(), 10), "1030");
-        assert_eq!(part_2_with_factor(sample_input.lines(), 100), "8410");
+        assert_eq!(
+            super::solve_with_expansion_factor(sample_input.lines(), 10),
+            "1030"
+        );
+        assert_eq!(
+            super::solve_with_expansion_factor(sample_input.lines(), 100),
+            "8410"
+        );
     }
 }
