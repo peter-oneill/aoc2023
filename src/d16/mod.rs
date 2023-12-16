dc288c920ddc9b46b4cdb11f3ec6f4c40b36c777
Peter O'Neill <github@onlypeter.com>
2023-12-16 19:46:24 +0000

    Bit faster

--- src/d16/mod.rs
+++ src/d16/mod.rs
@@ -1,359 +1,193 @@
-use std::{
-    borrow::BorrowMut,
-    cell::RefCell,
-    collections::{HashMap, HashSet},
-    str::Lines,
-};
+use std::str::Lines;
 
 use crate::Solver;
 pub struct Solver16;
+#[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
 
-#[derive(Debug)]
-struct Map {
-    x_size: isize,
-    y_size: isize,
-    grid: RefCell<Vec<Vec<Node>>>,
-    starts: HashMap<NodeExit, RefCell<bool>>,
+enum NodeType {
+    Empty,
+    Horizontal,
+    Vertical,
+    ForwardSlash,
+    BackSlash,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone)]
 enum Direction {
     N = 0,
     E,
     S,
     W,
-    None,
 }
+#[derive(Hash, Eq, PartialEq, Copy, Clone, Debug)]
 
-impl Direction {
-    fn opposite(&self) -> Self {
-        match self {
-            Direction::N => Direction::S,
-            Direction::E => Direction::W,
-            Direction::S => Direction::N,
-            Direction::W => Direction::E,
-            Direction::None => Direction::None,
-        }
-    }
-}
-#[derive(Debug)]
-
-enum NodeType {
-    Empty,
-    Vertical,
-    Horizontal,
-    ForwardSlash,
-    BackSlash,
-}
-#[derive(Debug)]
 struct Node {
-    // map: &'static Map,
-    x: isize,
-    y: isize,
     node_type: NodeType,
-    dir_visited: [bool; 5],
-    output_for_input: [[Direction; 2]; 4],
     lit: bool,
-    tested_entry_here: bool,
+    entered_this_way_before: [bool; 4],
 }
 
 impl Node {
-    fn new(x: usize, y: usize, c: char) -> Node {
-        let (node_type, output_for_input) = match c {
-            '.' => (
-                NodeType::Empty,
-                [
-                    [Direction::S, Direction::None],
-                    [Direction::W, Direction::None],
-                    [Direction::N, Direction::None],
-                    [Direction::E, Direction::None],
-                ],
-            ),
-            '-' => (
-                NodeType::Horizontal,
-                [
-                    [Direction::E, Direction::W],
-                    [Direction::W, Direction::None],
-                    [Direction::E, Direction::W],
-                    [Direction::E, Direction::None],
-                ],
-            ),
-            '|' => (
-                NodeType::Vertical,
-                [
-                    [Direction::S, Direction::None],
-                    [Direction::N, Direction::S],
-                    [Direction::N, Direction::None],
-                    [Direction::N, Direction::S],
-                ],
-            ),
-            '/' => (
-                NodeType::ForwardSlash,
-                [
-                    [Direction::W, Direction::None],
-                    [Direction::S, Direction::None],
-                    [Direction::E, Direction::None],
-                    [Direction::N, Direction::None],
-                ],
-            ),
-            '\\' => (
-                NodeType::BackSlash,
-                [
-                    [Direction::E, Direction::None],
-                    [Direction::N, Direction::None],
-                    [Direction::W, Direction::None],
-                    [Direction::S, Direction::None],
-                ],
-            ),
-            _ => panic!("Invalid node type"),
+    fn new(c: u8) -> Node {
+        let node_type = match c {
+            b'.' => NodeType::Empty,
+            b'-' => NodeType::Horizontal,
+            b'|' => NodeType::Vertical,
+            b'/' => NodeType::ForwardSlash,
+            b'\\' => NodeType::BackSlash,
+            _ => panic!("Unknown char {}", c),
         };
-
         Node {
-            // map,
-            x: x as isize,
-            y: y as isize,
             node_type,
-            output_for_input,
-            dir_visited: [false; 5],
             lit: false,
-            tested_entry_here: false,
+            entered_this_way_before: [false; 4],
         }
     }
+}
 
-    fn visit(&mut self, map: &Map, from_dir: Direction) -> [Direction; 2] {
-        // println!("Visiting {:?} from {:?}", self, from_dir);
-        if self.dir_visited[from_dir as usize] {
-            return [Direction::None, Direction::None];
-        }
-
-        self.lit = true;
-        self.dir_visited[from_dir as usize] = true;
-        let out_dirs = self.output_for_input[from_dir as usize];
-        self.dir_visited[out_dirs[0] as usize] = true;
-        self.dir_visited[out_dirs[1] as usize] = true;
-
-        // println!("out dirs: {:?}", out_dirs);
-
-        // if this is an entrance direction, mark the node as tested
-        if (self.x == 0 && from_dir == Direction::W)
-            || (self.x == map.y_size - 1 && from_dir == Direction::E)
-            || (self.y == 0 && from_dir == Direction::N)
-            || (self.y == map.x_size - 1 && from_dir == Direction::S)
-        {
-            *(*map
-                .starts
-                .get(
-                    &NodeExit {
-                        x: self.x,
-                        y: self.y,
-                        exit_dir: from_dir,
-                    }
-                    .to_node_entry(),
-                )
-                .unwrap())
-            .borrow_mut() = true;
-            // self.tested_entry_here = true;
-        }
-        out_dirs
-    }
+#[derive(Eq, PartialEq, Clone, Debug)]
+struct Map {
+    grid: Vec<Vec<Node>>,
+    max_x: isize,
+    max_y: isize,
 }
 
 impl Map {
+    fn new(grid: Vec<Vec<Node>>) -> Map {
+        let max_x = grid[0].len() as isize - 1;
+        let max_y = grid.len() as isize - 1;
+        Map { grid, max_x, max_y }
+    }
     fn reset(&mut self) {
-        let mut grid = self.grid.borrow_mut();
-        for row in grid.iter_mut() {
-            for node in row.iter_mut() {
+        self.grid.iter_mut().for_each(|row| {
+            row.iter_mut().for_each(|node| {
                 node.lit = false;
-                node.dir_visited = [false; 5];
+                node.entered_this_way_before = [false; 4];
+            })
+        });
+    }
+
+    fn get(&mut self, y: isize, x: isize) -> Option<&mut Node> {
+        self.grid
+            .get_mut::<usize>(y.try_into().ok()?)?
+            .get_mut::<usize>(x.try_into().ok()?)
+    }
+
+    fn traverse(&mut self, x: isize, y: isize, dir: Direction) -> usize {
+        let mut current_x = x;
+        let mut current_y = y;
+        let mut current_dir = dir;
+        let mut lit = 0;
+        loop {
+            match self.get(current_y, current_x) {
+                None => return lit,
+                Some(current_node) => {
+                    // Has this node already been lit when it had the same output direction before?
+                    if current_node.entered_this_way_before[current_dir as usize] {
+                        return lit;
+                    }
+                    current_node.entered_this_way_before[current_dir as usize] = true;
+
+                    if !current_node.lit {
+                        lit += 1;
+                    }
+
+                    current_node.lit = true;
+
+                    match (current_dir, &current_node.node_type) {
+                        (Direction::W, NodeType::Empty)
+                        | (Direction::W, NodeType::Horizontal)
+                        | (Direction::N, NodeType::BackSlash)
+                        | (Direction::S, NodeType::ForwardSlash) => {
+                            current_x -= 1;
+                            current_dir = Direction::W;
+                        }
+                        (Direction::E, NodeType::Empty)
+                        | (Direction::E, NodeType::Horizontal)
+                        | (Direction::N, NodeType::ForwardSlash)
+                        | (Direction::S, NodeType::BackSlash) => {
+                            current_x += 1;
+                            current_dir = Direction::E;
+                        }
+                        (Direction::N, NodeType::Empty)
+                        | (Direction::N, NodeType::Vertical)
+                        | (Direction::E, NodeType::ForwardSlash)
+                        | (Direction::W, NodeType::BackSlash) => {
+                            current_y -= 1;
+                            current_dir = Direction::N;
+                        }
+                        (Direction::S, NodeType::Empty)
+                        | (Direction::S, NodeType::Vertical)
+                        | (Direction::E, NodeType::BackSlash)
+                        | (Direction::W, NodeType::ForwardSlash) => {
+                            current_y += 1;
+                            current_dir = Direction::S;
+                        }
+                        (Direction::N, NodeType::Horizontal)
+                        | (Direction::S, NodeType::Horizontal) => {
+                            return lit
+                                + self.traverse(current_x - 1, current_y, Direction::W)
+                                + self.traverse(current_x + 1, current_y, Direction::E);
+                        }
+                        (Direction::E, NodeType::Vertical) | (Direction::W, NodeType::Vertical) => {
+                            return lit
+                                + self.traverse(current_x, current_y - 1, Direction::N)
+                                + self.traverse(current_x, current_y + 1, Direction::S);
+                        }
+                    }
+                }
             }
         }
     }
-    // Visits the node and returns a list of output directions _to check_.
-    // If the output directions are already lit, they aren't returned.
-    fn visit_node(&mut self, last_loc_and_bearing: NodeExit) -> Vec<NodeExit> {
-        // println!("Visited {:?}", last_loc_and_bearing);
-        let node_entry = last_loc_and_bearing.to_node_entry();
-        // println!("Visiting {:?}", node_entry);
-
-        if node_entry.x < 0
-            || node_entry.x >= self.x_size as isize
-            || node_entry.y < 0
-            || node_entry.y >= self.y_size as isize
-        {
-            return vec![];
-        }
-
-        let mut grid = self.grid.borrow_mut();
-        let this_node = grid
-            .get_mut(node_entry.y as usize)
-            .unwrap()
-            .get_mut(node_entry.x as usize)
-            .unwrap();
-        let next_dirs = this_node.visit(self, node_entry.exit_dir);
-
-        next_dirs
-            .iter()
-            .filter_map(|d| {
-                if d == &Direction::None {
-                    None
-                } else {
-                    Some(NodeExit {
-                        x: node_entry.x,
-                        y: node_entry.y,
-                        exit_dir: *d,
-                    })
-                }
-            })
-            .collect()
-    }
 }
-
-#[derive(Debug, Clone, Copy, Eq, Hash, PartialEq)]
-struct NodeExit {
-    x: isize,
-    y: isize,
-    exit_dir: Direction,
-}
-
-impl NodeExit {
-    fn to_node_entry(&self) -> NodeExit {
-        let (x, y) = match self.exit_dir {
-            Direction::N => (self.x, self.y - 1),
-            Direction::E => (self.x + 1, self.y),
-            Direction::S => (self.x, self.y + 1),
-            Direction::W => (self.x - 1, self.y),
-            Direction::None => panic!("Invalid direction"),
-        };
-
-        NodeExit {
-            x,
-            y,
-            exit_dir: self.exit_dir.opposite(),
-        }
-    }
-}
-
 impl Solver for Solver16 {
     fn day_number(&self) -> u32 {
         16
     }
 
     fn part1(&self, input_lines: Lines) -> String {
-        let mut map: Map = Map {
-            x_size: 0,
-            y_size: 0,
-            grid: RefCell::new(vec![]),
-            starts: HashMap::new(),
-        };
-        let mut mirror_grid = vec![];
-        for (line_ix, line) in input_lines.enumerate() {
-            mirror_grid.push(vec![]);
-            for (char_ix, char) in line.chars().enumerate() {
-                mirror_grid[line_ix].push(Node::new(char_ix, line_ix, char));
-            }
-        }
+        let grid: Vec<Vec<Node>> = input_lines
+            .map(|line| line.bytes().map(Node::new).collect())
+            .collect();
 
-        *map.grid.borrow_mut() = mirror_grid;
-        map.x_size = map.grid.borrow().len() as isize;
-        map.y_size = map.grid.borrow()[0].len() as isize;
-        map.starts.insert(
-            NodeExit {
-                x: -1,
-                y: 0,
-                exit_dir: Direction::E,
-            },
-            RefCell::new(false),
-        );
+        let mut map = Map::new(grid);
 
-        let starts_copy = map.starts.keys().map(|k| *k).collect::<Vec<NodeExit>>();
+        let lit = map.traverse(0, 0, Direction::E);
 
-        for start in starts_copy {
-            let mut current_loc_and_brearings = vec![start];
-
-            while let Some(loc) = current_loc_and_brearings.pop() {
-                current_loc_and_brearings.extend(map.visit_node(loc));
-            }
-        }
-
-        let grid = map.grid.borrow();
-        grid.iter().flatten().filter(|n| n.lit).count().to_string()
+        lit.to_string()
     }
 
     fn part2(&self, input_lines: Lines) -> String {
-        let mut map: Map = Map {
-            x_size: 0,
-            y_size: 0,
-            grid: RefCell::new(vec![]),
-            starts: HashMap::new(),
-        };
-        let mut mirror_grid = vec![];
-        for (line_ix, line) in input_lines.enumerate() {
-            mirror_grid.push(vec![]);
-            for (char_ix, char) in line.chars().enumerate() {
-                mirror_grid[line_ix].push(Node::new(char_ix, line_ix, char));
-            }
+        let grid: Vec<Vec<Node>> = input_lines
+            .map(|line| line.bytes().map(Node::new).collect())
+            .collect();
+
+        let mut map = Map::new(grid);
+
+        let x_len = map.grid[0].len() as isize;
+        let y_len = map.grid.len() as isize;
+
+        let mut starts = Vec::with_capacity(2 * map.grid.len() + 2 * map.grid[0].len());
+
+        for y in 0..y_len {
+            starts.push((0, y, Direction::E));
+            starts.push((x_len - 1, y, Direction::W));
         }
 
-        *map.grid.borrow_mut() = mirror_grid;
-        map.x_size = map.grid.borrow().len() as isize;
-        map.y_size = map.grid.borrow()[0].len() as isize;
-
-        for y in 0..map.y_size {
-            map.starts.insert(
-                NodeExit {
-                    x: -1,
-                    y,
-                    exit_dir: Direction::E,
-                },
-                RefCell::new(false),
-            );
-            map.starts.insert(
-                NodeExit {
-                    x: map.x_size,
-                    y,
-                    exit_dir: Direction::W,
-                },
-                RefCell::new(false),
-            );
-        }
-        for x in 0..map.x_size {
-            map.starts.insert(
-                NodeExit {
-                    x,
-                    y: -1,
-                    exit_dir: Direction::S,
-                },
-                RefCell::new(false),
-            );
-            map.starts.insert(
-                NodeExit {
-                    x,
-                    y: map.y_size,
-                    exit_dir: Direction::N,
-                },
-                RefCell::new(false),
-            );
+        for x in 0..x_len {
+            starts.push((x, 0, Direction::S));
+            starts.push((x, y_len - 1, Direction::N));
         }
 
-        let mut max = 0;
-
-        let starts_copy = map.starts.keys().map(|k| *k).collect::<Vec<NodeExit>>();
-
-        for start in starts_copy {
-            if map.starts[&start].borrow().clone() {
-                continue;
-            }
-            let mut current_loc_and_brearings = vec![start];
-            map.reset();
-
-            while let Some(loc) = current_loc_and_brearings.pop() {
-                current_loc_and_brearings.extend(map.visit_node(loc));
-            }
-            let new_val = map.grid.borrow().iter().flatten().filter(|n| n.lit).count();
-            max = std::cmp::max(max, new_val);
-        }
-
-        max.to_string()
+        starts
+            .iter()
+            .map(|(x, y, dir)| {
+                map.reset();
+                map.traverse(*x, *y, *dir)
+            })
+            .max()
+            .unwrap()
+            .to_string()
     }
 }
 
